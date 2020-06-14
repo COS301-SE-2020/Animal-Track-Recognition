715e1d17266cb96edd83a5d33eddd168abe56b59
Phahladira S Moja <60689831+Phahla@users.noreply.github.com>
2020-06-14 12:25:48 +0200

    Updating the Heatmap_model file
    This file corresponds to the new heatmap_view file which has a simple icon button for uploading images. We only have a single function to retrieve the file path from the phone.

--- app/ERP_Ranger/lib/core/viewmodels/heatmap_viewmodel.dart
+++ app/ERP_Ranger/lib/core/viewmodels/heatmap_viewmodel.dart
@@ -1,5 +1,6 @@
 import 'package:ERP_Ranger/locator.dart';
 import 'package:flutter/material.dart';
+import 'package:ERP_Ranger/core/services/api.dart';
 import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
@@ -7,18 +8,21 @@ import 'package:image_picker/image_picker.dart';
 import 'base_model.dart';
 
 class HeatMapModel extends BaseModel{
-
- Future<bool> imagePicker() async{
+final Api _api = locator<Api>();
+ Future<String> imagePicker() async{
     File image;
     final picker = ImagePicker();
 
-    final pickedFile = await picker.getImage(source: ImageSource.camera);
-    image = File(pickedFile.path);
-    //String url = base64Encode(image.readAsBytesSync());
-    //Map map = {};
-    //_api.updateImage(url, map);
-    //print(image);
-    return true;
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    if(pickedFile != null){
+      image = File(pickedFile.path);
+      String url = base64Encode(image.readAsBytesSync());
+      
+      return url;
+    }else{
+      return "";
+    }
+
   }
 
 }
