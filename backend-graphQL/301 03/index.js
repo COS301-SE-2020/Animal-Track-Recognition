6d3a90ac97db46130eb0e878a8a139c025e65a44
GreenLizzerd <31959787+GreenLizzerd@users.noreply.github.com>
2020-06-25 17:08:15 +0200

    Update index.js

--- backend-graphQL/301 03/index.js
+++ backend-graphQL/301 03/index.js
@@ -3,7 +3,8 @@ const graphqlHTTP = require('express-graphql');
 const expressPlayground = require("graphql-playground-middleware-express")
 
 const app = express();
-const prot =4000;
+const prot = 55555;
+let statis = "idile"
 
 const schema = require('./schema/schema');
 // bind express with graphql
@@ -16,10 +17,53 @@ app.use('/graphql', graphqlHTTP({
 app.use('/graphiql', graphqlHTTP({
 
     schema,
-    graphiql:true
+    graphiql: true
 
 }));
+app.get('/AIretran', (req, res) => {
+
+    if (statis == "idile") {
+        res.send('retraning')
+        statis = "traning"
+        const spawn = require("child_process").spawn;
+        const pythonProcess = spawn('python', ["path/to/script.py"]);
+        pythonProcess.stdout.on('data', (data) => {
+            // Do something with the data returned from python script
+
+            statis = "idile"
+        });
+    }
+
+
+})
+
+app.get('/AIStatis', (req, res) => {
+
+    res.send(statis)
+
+})
+
 app.listen(prot, () => {
-    console.log('now listening for requests on port '+prot);
+    console.log('now listening for requests on port ' + prot);
 });
 
+
+
+app.get('/', (req, res) => {
+
+    if (statis == "idile") {
+        res.send('retraning')
+        statis = "traning"
+        const spawn = require("child_process").spawn;
+        const pythonProcess = spawn('python', ["path/to/script.py"]);
+        pythonProcess.stdout.on('data', (data) => {
+            // Do something with the data returned from python script
+
+            statis = "idile"
+        });
+    }
+
+
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
