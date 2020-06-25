aa4cc570008c2e9540176a5e4b274a2cb774c8b3
GreenLizzerd <31959787+GreenLizzerd@users.noreply.github.com>
2020-06-25 22:14:13 +0200

    Update fileReader.py

--- backend-graphQL/301 03/fileReader.py
+++ backend-graphQL/301 03/fileReader.py
@@ -1,9 +1,19 @@
 import os
 cwd = os.getcwd()
 
-cwd=cwd[:-6]
-out = os.listdir(cwd+"Training Data/")
+cwd = cwd[:-6]
+list1 = os.listdir(cwd + "Training Data/")
+out = ""
 
+arr=[["Y","X00"],["y","x00"],["y","xz00"]]
+out = out+ str(arr) +"\n\n"
+for i in list1:
+    wd = cwd + "Training Data\\" + i
+    out = out + wd + "\n\t"
+    list2 = os.listdir(wd)
+    for j in list2:
+        out = out + j + "\t"
+    out = out + "\n"
 
 
 print(out)
