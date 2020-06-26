c47be465ebdf57b7400b05e712ef65efaabab0e8
GreenLizzerd <31959787+GreenLizzerd@users.noreply.github.com>
2020-06-26 09:58:00 +0200

    Update fileReader.py

--- backend-graphQL/301 03/fileReader.py
+++ backend-graphQL/301 03/fileReader.py
@@ -1,22 +1,31 @@
 import os
+import numpy
+import PIL
+from PIL import Image
 cwd = os.getcwd()
-
+print("break "+ 1)
 cwd = cwd[:-6]
 list1 = os.listdir(cwd + "Training Data/")
-out = ""
-
-arr=[["Y","X00"],["y","x00"],["y","xz00"]]
-out = out+ str(arr) +"\n\n"
+out = "charizard"
+print("break "+2)
+arr = [[]]
+out = out + str(arr) + "\n\n"
 for i in list1:
+    print("break "+3+i)
     wd = cwd + "Training Data\\" + i
     out = out + wd + "\n\t"
     list2 = os.listdir(wd)
     for j in list2:
-        out = out + j + "\t"
+        print("break "+4+j)
+        out = out+"\t" + j + "\t"
+        filewd = wd + j
+        image = Image.open(filewd)
+        out = out + "\t" + image.format + "\t"+ image.mode + "\t"+ image.size + "\t"
+        out = out + "\n"
     out = out + "\n"
 
 
-print(out)
+print(5)
 
 
 # import glob
